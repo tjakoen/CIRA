ec1885b3a902488244b886f595dbc7fbf734430e
Tjakoen Stolk <tjakoen@Antonettes-MacBook-Pro.local>
2018-12-19 22:04:12 +0800

    Started with edit functionality for reports

--- src/pages/reports/report-details/report-details.ts
+++ src/pages/reports/report-details/report-details.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { ViewController, normalizeURL, ToastController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+import { ViewController, normalizeURL, ToastController, NavParams, AlertController, LoadingController, ModalController } from 'ionic-angular';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { FirebaseService } from '../../services/firebase.service';
 import { ImagePicker } from '@ionic-native/image-picker';
+import { NewReportModalPage } from '../new-report-modal/new-report-modal';
 
 @Component({
   templateUrl: 'report-details.html'
@@ -21,7 +22,8 @@ export class ReportDetailPage {
     private formBuilder: FormBuilder,
     private imagePicker: ImagePicker,
     private firebaseService: FirebaseService,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    public modalCtrl: ModalController,
   ) {
     this.loading = this.loadingCtrl.create();
   }
@@ -32,7 +34,6 @@ export class ReportDetailPage {
 
   getData(){
     this.report = this.navParams.get('data');
-    console.log(this.report)
     this.validations_form = this.formBuilder.group({
       title: new FormControl(this.report.title, Validators.required),
       description: new FormControl(this.report.description, Validators.required)
@@ -43,7 +44,13 @@ export class ReportDetailPage {
    this.viewCtrl.dismiss();
   }
 
-
-
-
+  openNewReportModal(){
+    let modal = this.modalCtrl.create(NewReportModalPage, {data: this.report});
+    modal.onDidDismiss( data => {
+      if ( typeof data != 'undefined' ) {
+        this.dismiss();
+      }
+    });
+    modal.present();
+  }
 }
